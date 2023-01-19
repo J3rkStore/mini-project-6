ba1f590bbb5b97837927f1ab6f7f109a0ea765c2
Kyle Simmons <simmonskyleb@gmail.com>
2023-01-18 21:02:53 -0700

    handling script.js vars

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -8,9 +8,11 @@ function handleSearchFormSubmit(event) {
   // TODO create a variable for formatInputVal that obtains the value for the format input
 
   // TODO if the user does not enter a search input, present an error in the console prompting them that they need a search input value
-
+  if (!searchInputVal){
+    alert("Search field cannot be blank")
+  }
   // TODO Adujust the var queryString so that the query url uses the var searchInputVal after q= and the var formatInputVal after &format=
-  var queryString = "./search-results.html?q=&format=";
+  var queryString = "./search-results.html?q=" + searchInputVal + "&format=" + formatInputVal
 
   location.assign(queryString);
 }
