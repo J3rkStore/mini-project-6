4f77a1d43337b44cee96a012ecec6113e05f18da
jerk-store <taran.leu@outlook.com>
2023-01-18 21:07:22 -0700

    doing it

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,8 +4,9 @@ function handleSearchFormSubmit(event) {
   event.preventDefault();
 
   // TODO create a variable for searchInputVal that obtains the value for the search input
-
+  var searchInputVal = searchFormEl.value;
   // TODO create a variable for formatInputVal that obtains the value for the format input
+  var formatInputVal = formatEl.value;
 
   // TODO if the user does not enter a search input, present an error in the console prompting them that they need a search input value
 
